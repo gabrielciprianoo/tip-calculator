a5ade0c5f47cb5b84178d195bbde407070cb29d4
gabrielciprianoo <devgabrielcipriano@gmail.com>
2025-03-12 16:54:23 -0600

    initial config

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { menuItems } from "./data/db";
+
+function App() {
+  return (
+    <>
+      <header className="bg-blue-600 p-10 flex justify-center items-center"> 
+        <h1 className="text-3xl font-semibold uppercase text-gray-100">Calculadora de propinas</h1>
+      </header>
+    </>
+  );
+}
+
+export default App;

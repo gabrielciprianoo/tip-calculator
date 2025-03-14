dccd294a28b882ae9200658ebb5894b1a43238a5
gabrielciprianoo <devgabrielcipriano@gmail.com>
2025-03-14 17:53:58 -0600

    feature: function addItem added

--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -1,11 +1,15 @@
 import type { MenuItem } from "../types";
 
 type MenuItemProps = {
-  item: MenuItem;
+  item: MenuItem,
+  addItem: (item: MenuItem) => void;
 };
-export default function MenuItem({ item }: MenuItemProps) {
+export default function MenuItem({ item, addItem }: MenuItemProps) {
   return (
-    <button className="flex justify-between border p-1 border-blue-400 hover:bg-blue-200 p-2 cursor-pointer w-full ">
+    <button
+     className="flex justify-between border border-blue-600 hover:bg-blue-200 p-2 cursor-pointer w-full "
+     onClick={() => addItem(item)}
+     >
       <h2>{item.name}</h2>
       <h2 className="font-bold">${item.price}</h2>
     </button>
